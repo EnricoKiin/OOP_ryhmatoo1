00a77650e7c5b09b00a2a4480e3dc801c17106d4
tim? <timmattias.juksaar@gmail.com>
2026-03-17 12:37:10 +0200

    vastane moku

---
+++ src/Möku.java
@@ -0,0 +1,12 @@
+public class Möku implements Vastane {
+
+    private int health = 50;
+
+    private int rynde_tugevus = 5;
+
+    public int ryndab() {};
+
+    public int getElud() {};
+
+    public void setElud() {};
+}
