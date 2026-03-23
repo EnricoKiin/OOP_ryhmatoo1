0f431a767a61cf05bafebca58eafebee78479cd6
tim? <timmattias.juksaar@gmail.com>
2026-03-23 19:07:27 +0200

    meetod looBaar

--- src/Baar.java
+++ src/Baar.java
@@ -1,12 +1,17 @@
 public class Baar extends Vastane {
 
-    public Baar(String nimi, int elud, double kaitsePrtosent, int rynda_dmg) {
-        super(nimi, elud, kaitsePrtosent, rynda_dmg);
+    public Baar(String nimi, int elud, double kaitseProtsent, int rynda_dmg) {
+        super(nimi, elud, kaitseProtsent, rynda_dmg);
     }
     @Override
     public void ryndeBoost() {
         super.ryndeBoost();
 
         System.out.println(this.toString() + " turva ei lase sind välja, pead joogi ostma.");
+    }
+
+    @Override
+    public boolean kaotaElud(int dmg) {
+        return super.kaotaElud(dmg);
     }
 }
--- src/Testryhmatoo.java
+++ src/Testryhmatoo.java
@@ -12,4 +12,36 @@ public class Testryhmatoo {
 
         tudeng.setNimi(nimi);
     }
+
+
+    public Baar looBaar(String nimi, int raskusTase) {
+        int elud=0;
+        double kaitseProtsent=0.0;
+        int rynda_dmg=0;
+
+        switch (raskusTase) {
+
+            case 1:
+                elud = (int) (Math.random() * 8) + 10;
+                kaitseProtsent = Math.random() * 0.2 + 0.1;
+                rynda_dmg = (int) Math.random() * 2 + 2;
+                break;
+
+            case 2:
+                elud = (int) Math.random() * 8 + 15;
+                kaitseProtsent = Math.random() * 0.2 + 0.3;
+                rynda_dmg = (int) Math.random() * 2 + 3;
+                break;
+
+            case 3:
+                elud = (int) Math.random() * 9 + 18;
+                kaitseProtsent = Math.random() * 0.2 + 0.4;
+                rynda_dmg = (int) Math.random() * 2 + 4;
+                break;
+
+        }
+
+        Baar bar = new Baar(nimi, elud, kaitseProtsent, rynda_dmg);
+        return bar;
+    }
 }
